ef1541dff6197d1c5b36a3de440506029dde8660
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2024-01-11 00:42:12 +0530

    Errors hi errors

---
+++ client/android/app/src/main/kotlin/com/face/face_alexa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.face.face_alexa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ client/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:face_alexa/pages/speech_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MyApp2());
+}
